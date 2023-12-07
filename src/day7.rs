083cb3d9712c0a504279a1005a8c6139699eb020
Christopher-Zhang <chrisconcord@gmail.com>
2023-12-07 02:03:38 -0500

    Fixed part 2, don't know why it wasn't working before, but this is much smarter

--- src/day7.rs
+++ src/day7.rs
@@ -43,7 +43,7 @@ pub async fn advent_2(data: String) -> u64 {
     order.sort_by(|a, b| {
         let mut ordering = a.0.cmp(&b.0);
         let mut index = 0;
-        while ordering == Ordering::Equal && index < 6 {
+        while ordering == Ordering::Equal && index < 5 {
             let c = get_priority_2(a.1.chars().nth(index).unwrap());
             let d = get_priority_2(b.1.chars().nth(index).unwrap());
             ordering = c.cmp(&d);
@@ -52,11 +52,11 @@ pub async fn advent_2(data: String) -> u64 {
         return ordering;
     });
 
-    for i in 0 .. 1000 {
+    for i in 0 .. order.len() {
         let x = order.get(i).unwrap();
         if x.1.chars().collect::<Vec<char>>().contains(&'J') && x.0 == 6 {
 
-            // dbg!(order.get(i));
+            dbg!(order.get(i));
         }
     }
     for (i, hand) in order.iter().enumerate() {
@@ -106,59 +106,35 @@ fn parse_hand(hand: &str) -> u64 {
 
 }
 fn parse_hand_2(hand: &str) -> u64 {
-    let mut counts = [0; 13];
+    let mut counts: [u64; 13] = [0; 13];
     let mut j_count = 0;
     for c in hand.chars() {
         if c == 'J' {
             j_count += 1;
         }
-        else {
+        else 
+        {
             let index = get_priority_2(c);
             counts[index as usize] += 1;
         }
     }
-    if j_count > 5 {
-        dbg!(counts);
-    }
-    // print!("{}", j_count);
-    let mut pair_count = 0;
-    let mut set_count = 0;
-    let mut card = 20;
-    for (_, count) in counts.iter().enumerate() {
-        if count + j_count == 5 {
-            return 6;
-        }
-        if count + j_count == 4 {
-            return 5;
-        }
 
+    counts.sort_by(|a,b| b.cmp(&a));
+    let first = counts[0];
+    let second = counts[1];
+    if first + j_count > 3 {
+        return first + j_count + 1;
     }
-    for (i, count) in counts.iter().enumerate() {
-        if count + j_count == 3 {
-            j_count = 0;
-            card = i;
-            set_count += 1;
-        }
-    }
-    for (i, count) in counts.iter().enumerate() {
-        if count + j_count == 2 && i != card {
-            j_count = 0;
-            pair_count += 1;
-        }   
-    }
-
-    if set_count == 1 {
-        if pair_count == 1 {
-            return 4;
+    if first + j_count == 3 {
+        if second == 2 {
+            return 4
         }
         return 3;
     }
-    
-    if pair_count == 2 {
-        return 2;
-    }
-
-    if pair_count == 1 {
+    if first + j_count == 2 {
+        if second == 2 {
+            return 2;
+        }
         return 1;
     }
     return 0;
@@ -172,6 +148,7 @@ fn get_priority(c: char) -> u64 {
 }
 
 fn get_priority_2(c: char) -> u64 {
+    // return get_priority(c);
     let priority = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
     priority.iter().position(|&r| r == c).unwrap_or(0) as u64
 }
